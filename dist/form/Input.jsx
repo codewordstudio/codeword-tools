e4c84cbc66ae2d83f3b57b9a526699cc5030e390
Raghav Joshi <ragzor9999@gmail.com>
2019-04-11 23:19:09 +0530

    removed npm completely and converted to submodules

--- dist/form/Input.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-const Input = ({ children, onClick }) => (<input type="button" onClick={onClick}>
-		{children}
-	</input>);
-export default Input;
-//# sourceMappingURL=Input.jsx.map
