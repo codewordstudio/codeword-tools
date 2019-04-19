c3a690419df1b45f7a5ad802ec563e921f4f3b2d
Raghav Joshi <ragzor9999@gmail.com>
2019-04-19 16:54:56 +0530

    Modifications

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -1,137 +1,139 @@
-import React, { Component } from 'react';
-import { Formik, Field, FormikProps, FieldProps, Form } from 'formik';
-import * as Yup from 'yup';
-import { withApollo } from 'react-apollo';
-import { signup, authorizeWithGoogle } from '../../tools/auth/auth0';
-import styled from 'styled-components';
-import Button from '../../../components/Button';
-import SocialButton from '../../../components/SocialButton';
-import Input from '../form/Input';
+import React, { Component } from "react";
+import { Formik, Field, FormikProps, FieldProps, Form } from "formik";
+import * as Yup from "yup";
+import { withApollo } from "react-apollo";
+import { signup, authorizeWithGoogle } from "../../tools/auth/auth0";
+import styled from "styled-components";
+import Button from "../../../components/Button";
+import SocialButton from "../../../components/SocialButton";
+import Input from "../form/Input";
 
 const StyledFormDivider = styled.div`
-	z-index: 0;
-	position: relative;
-	text-align: center;
-	margin: 25px 0;
-	&:before {
-		content: '';
-		position: absolute;
-		top: 50%;
-		left: 0;
-		right: 0;
-		border-bottom: 1px solid #e7e7e7;
-		z-index: -1;
-	}
+  z-index: 0;
+  position: relative;
+  text-align: center;
+  margin: 25px 0;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    border-bottom: 1px solid #e7e7e7;
+    z-index: -1;
+  }
 `;
 const StyledFormDividerContent = styled.div`
-	display: inline-block;
-	padding: 0 0.5em;
-	background: #fff;
+  display: inline-block;
+  padding: 0 0.5em;
+  background: #fff;
 `;
 
-interface SignupProps {}
+interface SignupProps {
+  client: any;
+}
 interface SignupState {}
 
 interface SignupFormValues {
-	name: string;
-	email: string;
-	password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 // @todo convert it to SFC
 class Signup extends Component<SignupProps, SignupState> {
-	SignupSchema = Yup.object().shape({
-		name: Yup.string().required('Please fill out your name!'),
-		email: Yup.string()
-			.email('Oops! That looks like an invalid Email Address!')
-			.required('Please fill out your email address!'),
-		password: Yup.string().required('Please fill out your password!'),
-	});
+  SignupSchema = Yup.object().shape({
+    name: Yup.string().required("Please fill out your name!"),
+    email: Yup.string()
+      .email("Oops! That looks like an invalid Email Address!")
+      .required("Please fill out your email address!"),
+    password: Yup.string().required("Please fill out your password!")
+  });
 
-	render() {
-		return (
-			<Formik
-				initialValues={{
-					name: '',
-					email: '',
-					password: '',
-				}}
-				validationSchema={this.SignupSchema}
-				// Validate only on submit
-				validateOnChange={false}
-				validateOnBlur={false}
-				onSubmit={(values, { setSubmitting, setStatus }) => {
-					let { email, password, name } = values;
-					// Try to Signup the user, if Signup doesn't happen throw the error
-					signup(email, password, name, this.props.client).catch(err => {
-						console.log(err);
-					});
-					setSubmitting(false);
-				}}
-				render={(formikBag: FormikProps<SignupFormValues>) => (
-					<React.Fragment>
-						<SocialButton onClick={authorizeWithGoogle} platform="google">
-							Sign Up With Google
-						</SocialButton>
-						<Form>
-							<StyledFormDivider>
-								<StyledFormDividerContent>Or</StyledFormDividerContent>
-							</StyledFormDivider>
-							<Field
-								name="name"
-								render={({ field, form }: FieldProps<SignupFormValues>) => {
-									return (
-										<Input
-											name="name"
-											type="text"
-											label="Your Name"
-											field={field}
-											error={form.errors.name}
-										/>
-									);
-								}}
-							/>
-							<Field
-								name="email"
-								render={({ field, form }: FieldProps<SignupFormValues>) => {
-									return (
-										<Input
-											name="email"
-											type="text"
-											label="Email"
-											field={field}
-											error={form.errors.email}
-										/>
-									);
-								}}
-							/>
-							<Field
-								name="password"
-								render={({ field, form }: FieldProps<SignupFormValues>) => (
-									<Input
-										name="password"
-										type="password"
-										label="Password"
-										field={field}
-										error={form.errors.password}
-									/>
-								)}
-							/>
-							<Button
-								color="black"
-								type="submit"
-								design="solid"
-								large={true}
-								full={true}
-								disabled={formikBag.isSubmitting}
-							>
-								Register
-							</Button>
-						</Form>
-					</React.Fragment>
-				)}
-			/>
-		);
-	}
+  render() {
+    return (
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: ""
+        }}
+        validationSchema={this.SignupSchema}
+        // Validate only on submit
+        validateOnChange={false}
+        validateOnBlur={false}
+        onSubmit={(values, { setSubmitting, setStatus }) => {
+          let { email, password, name } = values;
+          // Try to Signup the user, if Signup doesn't happen throw the error
+          signup(email, password, name, this.props.client).catch(err => {
+            console.log(err);
+          });
+          setSubmitting(false);
+        }}
+        render={(formikBag: FormikProps<SignupFormValues>) => (
+          <React.Fragment>
+            <SocialButton onClick={authorizeWithGoogle} platform="google">
+              Sign Up With Google
+            </SocialButton>
+            <Form>
+              <StyledFormDivider>
+                <StyledFormDividerContent>Or</StyledFormDividerContent>
+              </StyledFormDivider>
+              <Field
+                name="name"
+                render={({ field, form }: FieldProps<SignupFormValues>) => {
+                  return (
+                    <Input
+                      name="name"
+                      type="text"
+                      label="Your Name"
+                      field={field}
+                      error={form.errors.name}
+                    />
+                  );
+                }}
+              />
+              <Field
+                name="email"
+                render={({ field, form }: FieldProps<SignupFormValues>) => {
+                  return (
+                    <Input
+                      name="email"
+                      type="text"
+                      label="Email"
+                      field={field}
+                      error={form.errors.email}
+                    />
+                  );
+                }}
+              />
+              <Field
+                name="password"
+                render={({ field, form }: FieldProps<SignupFormValues>) => (
+                  <Input
+                    name="password"
+                    type="password"
+                    label="Password"
+                    field={field}
+                    error={form.errors.password}
+                  />
+                )}
+              />
+              <Button
+                color="black"
+                type="submit"
+                design="solid"
+                large={true}
+                full={true}
+                disabled={formikBag.isSubmitting}
+              >
+                Register
+              </Button>
+            </Form>
+          </React.Fragment>
+        )}
+      />
+    );
+  }
 }
 
 export default withApollo(Signup);
--- form/Input.tsx
+++ form/Input.tsx
@@ -1,84 +1,84 @@
-import * as React from 'react';
-import styled from 'styled-components';
+import * as React from "react";
+import styled from "styled-components";
 
 export const ResetInput = styled.input`
-	flex-shrink: 1;
-	border: 0;
-	padding: 8px;
-	display: inline-block;
-	vertical-align: middle;
-	white-space: normal;
-	background: none;
-	line-height: 1;
+  flex-shrink: 1;
+  border: 0;
+  padding: 8px;
+  display: inline-block;
+  vertical-align: middle;
+  white-space: normal;
+  background: none;
+  line-height: 1;
 
-	/* Browsers have different default form fonts */
-	font-size: inherit;
-	font-family: inherit;
+  /* Browsers have different default form fonts */
+  font-size: inherit;
+  font-family: inherit;
 
-	&:focus {
-		outline: initial;
-	}
+  &:focus {
+    outline: initial;
+  }
 `;
 
 export const FloatingInput: any = styled(ResetInput)`
-	width: 100%;
-	height: 56px;
-	padding-left: 15px; /*  Should be same as Label Margin left */
-	font-size: 14px;
-	padding-top: 29px;
-	position: relative;
-	border: 1px solid;
-	border-color: ${({ error }: any) => (error ? 'red' : '#e7e7e7')};
-	transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
+  width: 100%;
+  height: 56px;
+  padding-left: 15px; /*  Should be same as Label Margin left */
+  font-size: 14px;
+  padding-top: 29px;
+  position: relative;
+  border: 1px solid;
+  border-color: ${({ error }: any) => (error ? "red" : "#e7e7e7")};
+  transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
 
-	&:focus {
-		border-color: #000;
-	}
+  &:focus {
+    border-color: #000;
+  }
 
-	&:focus + label,
-	&:not(:placeholder-shown) + label {
-		/* &:valid + label { */
-		transform: translateY(-12px);
-		font-size: 75%;
-	}
+  &:focus + label,
+  &:not(:placeholder-shown) + label {
+    /* &:valid + label { */
+    transform: translateY(-12px);
+    font-size: 75%;
+  }
 `;
 
-export const FloatingLabel = styled.label`
-	position: absolute;
-	font-size: 14px;
-	color: ${({ error }: any) => (error ? 'red' : '')};
-	margin-left: 15px; /* Should be same as input margin left */
-	line-height: 56px;
-	/* To also make the label clickable */
-	// @todo label is not clickable because z-index is not -1
-	z-index: 1;
-	transition: all 150ms ease;
+export const FloatingLabel: any = styled.label`
+  position: absolute;
+  font-size: 14px;
+  color: ${({ error }: any) => (error ? "red" : "")};
+  margin-left: 15px; /* Should be same as input margin left */
+  line-height: 56px;
+  /* To also make the label clickable */
+  // @todo label is not clickable because z-index is not -1
+  z-index: 1;
+  transition: all 150ms ease;
 `;
 
 export const StyledInputWrapper = styled.div`
-	display: flex;
-	margin-bottom: 20px;
+  display: flex;
+  margin-bottom: 20px;
 `;
 
 interface Props {
-	/**
-	 * Click event handler
-	 * @default null
-	 */
-	name: string;
-	label: string;
-	field: {};
-	type: string;
-	error: string;
+  /**
+   * Click event handler
+   * @default null
+   */
+  name: string;
+  label: string;
+  field: {};
+  type: string;
+  error: string;
 }
 
 const Input: React.SFC<Props> = ({ name, label, field, type, error }) => (
-	<StyledInputWrapper>
-		<FloatingInput error={error} type={type} placeholder=" " {...field} />
-		<FloatingLabel error={error} htmlFor={name}>
-			{error || label}
-		</FloatingLabel>
-	</StyledInputWrapper>
+  <StyledInputWrapper>
+    <FloatingInput error={error} type={type} placeholder=" " {...field} />
+    <FloatingLabel error={error} htmlFor={name}>
+      {error || label}
+    </FloatingLabel>
+  </StyledInputWrapper>
 );
 
 export default Input;
